197b84f8ed088e59f9b01befef8d739d29bb164d
Faheem <Faheemk793@gmail.com>
2024-12-23 16:25:43 +0530

    fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import HeroSection from "./(Home Page)/HeroSection";
 import AboutUs from "./(Home Page)/AboutUs";
 import TechStack from "./(Home Page)/TechStack";
-import Portfolio from "./(Home Page)/Portfolio";
+ 
 import StackCard from "./(Home Page)/StackCard";
 import { CompantStats } from "./(Home Page)/CompantStats";
 export default function Home() {
