8e294a08950c8c5b2b773fc1f284bf6b6d602dde
faheem221 <faheemk793@gmail.com>
2025-03-22 15:02:56 +0530

    fixed errors

--- app/App chunks/components/Logo.tsx
+++ app/App chunks/components/Logo.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Link from "next/link";
 
 const Logo = ({ className, source = '/Logo.png' }: { className?: string, source?: string }) => {
+  console.log(source)
   return (
     <Link href="/">
       <div className={`w-[220px] text-slate-50 ${className}`}>
