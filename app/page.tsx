564dd3d6e49347bd4923a5717f8d6cb37ad28d87
faheem221 <faheemk793@gmail.com>
2025-03-22 14:56:21 +0530

    fixed errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 "use client";
 import HeroSection from "./(Home Page)/HeroSection";
-import AboutUs from "./(Home Page)/AboutUs";
 import TechStack from "./(Home Page)/TechStack";
 import { CompantStats } from "./(Home Page)/CompantStats";
-import MaskSection from "./(Home Page)/MaskSection";
-import InstaVideos from "./(Home Page)/InstaVideos";
+
 import Slider from "./(Home Page)/Slider";
 import Form from "./App chunks/components/Form";
 import { AnimatePresence, motion } from "framer-motion";
